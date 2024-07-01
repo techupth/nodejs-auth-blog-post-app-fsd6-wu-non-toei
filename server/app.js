5547e6555a7581d2b02172496a3b8488d84ba95d
Apirak Fakin <apirakfakin@gmail.com>
2024-07-01 13:45:06 +0700

    feat: add ex-1

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import postRouter from "./apps/posts.js";
+import authRouter from "./apps/auth.js";
 import { client } from "./utils/db.js";
 
 async function init() {
@@ -13,6 +14,7 @@ async function init() {
   app.use(cors());
   app.use(bodyParser.json());
   app.use("/posts", postRouter);
+  app.use("/register", authRouter);
 
   app.get("/", (req, res) => {
     res.send("Hello World!");
--- server/apps/auth.js
+++ server/apps/auth.js
@@ -1,9 +1,37 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
 
 const authRouter = Router();
 
 // 🐨 Todo: Exercise #1
 // ให้สร้าง API เพื่อเอาไว้ Register ตัว User แล้วเก็บข้อมูลไว้ใน Database ตามตารางที่ออกแบบไว้
+authRouter.post("/", async (req, res) => {
+  const user = {
+    username: req.body.username,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  };
+  if (
+    !req.body.username ||
+    !req.body.password ||
+    !req.body.firstName ||
+    !req.body.lastName
+  ) {
+    return res.status(400).json({
+      message: "data missing",
+    });
+  }
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  const collection = db.collection("users");
+  await collection.insertOne(user);
+  return res.status(200).json({
+    message: "User has been created successfully",
+  });
+});
 
 // 🐨 Todo: Exercise #3
 // ให้สร้าง API เพื่อเอาไว้ Login ตัว User ตามตารางที่ออกแบบไว้
