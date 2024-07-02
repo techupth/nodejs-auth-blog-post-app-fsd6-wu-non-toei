22231b850df5aff59c81cf3a6a9d6ec54f2006fd
RcnonS <ratchanon.somprasong@gmail.com>
2024-07-02 16:15:01 +0700

    feat: created logout feature and midleware function

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import usePosts from "../hooks/usePosts";
 import getPublishedDate from "../utils/getPublishedDate";
+import { useAuth } from "../contexts/authentication";
 
 function HomePage() {
   const navigate = useNavigate();
@@ -15,6 +16,8 @@ function HomePage() {
   useEffect(() => {
     getPosts({ status, keywords, page });
   }, [status, keywords, page]);
+
+  const { logout } = useAuth();
 
   return (
     <div>
@@ -31,7 +34,13 @@ function HomePage() {
            // 🐨 Todo: Exercise #7
           //  นำ Function `logout` จาก AuthContext มา Execute ใน Prop `onClick`
         */}
-        <button>Logout</button>
+        <button
+          onClick={() => {
+            logout();
+          }}
+        >
+          Logout
+        </button>
       </div>
       <div className="search-box-container">
         <div className="search-box">
